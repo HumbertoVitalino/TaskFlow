412393ef7ed98a20ef917566c2b6a19151924227
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-21 20:36:09 -0300

    feat: service extension

---
+++ Core/Services/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Core.Services;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureApplicationApp(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+    }
+}
