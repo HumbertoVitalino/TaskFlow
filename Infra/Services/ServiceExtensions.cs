b547766121db78b3396e404371b66ed4f49f8103
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-22 07:23:13 -0300

    feat: serviceextension

---
+++ Infra/Services/ServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Core.Interfaces;
+using Infra.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.Services;
+
+public static class ServiceExtensions
+{
+    public static void ConfigurePersistenceApp(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("SqlServer");
+    }
+}
+
