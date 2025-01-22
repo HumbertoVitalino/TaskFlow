635adbee0992dba5cb3844132d87696a4e700e85
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-22 08:33:22 -0300

    feat: create task usecase

---
+++ Core/UseCases/TaskUseCases/CreateTask.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Core.Interfaces;
+using Core.UseCases.TaskUseCases.Boundaries;
+using MediatR;
+
+namespace Core.UseCases.TaskUseCases;
+
+public class CreateTask : IRequestHandler<CreateTaskRequest, CreateTaskResponse>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ITaskRepository _taskRepository;
+    private readonly IMapper _mapper;
+
+    public CreateTask(IUnitOfWork unitOfWork, ITaskRepository taskRepository, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _taskRepository = taskRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<CreateTaskResponse> Handle(CreateTaskRequest request, CancellationToken cancellationToken)
+    {
+        var task = _mapper.Map<Entities.Task>(request);
+
+        _taskRepository.Create(task);
+
+        await _unitOfWork.Commit(cancellationToken);
+
+        return _mapper.Map<CreateTaskResponse>(task);
+    }
+}
