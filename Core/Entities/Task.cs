d6ce3a6293efa3376a42f69d92c0e16f8d6590ee
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-22 08:33:09 -0300

    refactor: possible null

--- Core/Entities/Task.cs
+++ Core/Entities/Task.cs
@@ -4,7 +4,7 @@ public sealed class Task : BaseEntity
 {
     public int Id { get; set; }
     public string Title { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public DateTime DueDate { get; set; }
     public string Status { get; set; }
 }
