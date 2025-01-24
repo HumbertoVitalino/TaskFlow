ff14252fc227070bdff49d2dc6a111abc94b0aef
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-24 15:51:04 -0300

    refactoring: to enum

--- Infra/Repositories/TaskRepository.cs
+++ Infra/Repositories/TaskRepository.cs
@@ -1,27 +1,11 @@
 ﻿using Core.Interfaces;
 using Infra.Context;
-using Microsoft.EntityFrameworkCore;
 
 namespace Infra.Repositories
 {
     public class TaskRepository : BaseRepository<Core.Entities.Task>, ITaskRepository
     {
         public TaskRepository(AppDbContext context) : base(context) { }
-
-        public async Task<Core.Entities.Task> UpdateStatus(int status, int taskId, CancellationToken cancellationToken)
-        {
-            var task = await _context.Set<Core.Entities.Task>().FirstOrDefaultAsync(t => t.Id == taskId, cancellationToken);
-
-            if (task == null)
-            {
-                throw new KeyNotFoundException("Task not found.");
-            }
-
-            task.Status = status;
-
-            _context.Set<Core.Entities.Task>().Update(task);
-
-            return task;
-        }
+        
     }
 }
