8a1625c0cd25245234bfab8ead48dda44969c027
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-24 16:51:13 -0300

    feat: controller

---
+++ API/Controllers/TagController.cs
@@ -0,0 +1,26 @@
+﻿using Core.UseCases.TagUseCases.CreateTag.Boundaries;
+using Core.UseCases.TagUseCases.Output;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TagController : ControllerBase
+{
+    private readonly IMediator _mediatr;
+
+    public TagController(IMediator mediatr)
+    {
+        _mediatr = mediatr;
+    }
+
+    [HttpPost("CreateTag")]
+    public async Task<ActionResult<TagOutput>> Create(CreateTagInput input, CancellationToken cancellationToken)
+    {
+        var output = await _mediatr.Send(input, cancellationToken);
+        return Ok(output);
+    }
+
+}
