5aae3c1ee239a789548db751a3da29cc32f883d8
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-24 10:06:02 -0300

    feat: boundaries

---
+++ Core/UseCases/TaskUseCases/UpdateTask/Boundaries/UpdateTaskInput.cs
@@ -0,0 +1,6 @@
+﻿using Core.UseCases.TaskUseCases.Output;
+using MediatR;
+
+namespace Core.UseCases.TaskUseCases.UpdateTask.Boundaries;
+
+public sealed record UpdateTaskInput(int Id, string Title, string Description, DateTime DueDate) : IRequest<TaskOutput>;
