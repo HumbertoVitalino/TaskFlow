e368dfade8b3c7c78807ccf3c38648fdc467e63a
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-24 08:02:30 -0300

    feat: getall tasks usecase

---
+++ Core/UseCases/TaskUseCases/GetAllTasks/Boundaries/GetAllTasksInput.cs
@@ -0,0 +1,6 @@
+﻿using Core.UseCases.TaskUseCases.Output;
+using MediatR;
+
+namespace Core.UseCases.TaskUseCases.GetAllTasks.Boundaries;
+
+public sealed record GetAllTasksInput : IRequest<List<TaskOutput>>;
---
+++ Core/UseCases/TaskUseCases/GetAllTasks/Mapper/GetAllTasksMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core.UseCases.TaskUseCases.GetAllTasks.Boundaries;
+using Core.UseCases.TaskUseCases.Output;
+
+namespace Core.UseCases.TaskUseCases.GetAllTasks.Mapper;
+
+public sealed class GetAllTasksMapper : Profile
+{
+    public GetAllTasksMapper()
+    {
+        CreateMap<GetAllTasksInput, Entities.Task>();
+        CreateMap<Entities.Task, TaskOutput>();
+    }
+}
