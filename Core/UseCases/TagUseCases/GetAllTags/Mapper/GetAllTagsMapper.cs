49861536510f4ea78dacc77da52bff952186bb25
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-24 17:18:10 -0300

    feat: boundaries

---
+++ Core/UseCases/TagUseCases/GetAllTags/Mapper/GetAllTagsMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Core.UseCases.TagUseCases.GetAllTags.Boundaries;
+using Core.UseCases.TagUseCases.Output;
+
+namespace Core.UseCases.TagUseCases.GetAllTags.Mapper;
+
+public sealed class GetAllTagsMapper : Profile
+{
+    public GetAllTagsMapper()
+    {
+        CreateMap<GetAllTagsInput, Tag>();
+        CreateMap<Tag, TagOutput>();
+    }
+}
