9384398361d1744a76dd5c10876333fd0f5cd008
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-26 09:04:19 -0300

    feat: usecase

---
+++ Core/UseCases/TaskUseCases/AddTagToTask/AddTagToTask.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Core.Interfaces;
+using Core.UseCases.TaskUseCases.AddTagToTask.Boundaries;
+using Core.UseCases.TaskUseCases.Output;
+using MediatR;
+
+namespace Core.UseCases.TaskUseCases.AddTagToTask;
+
+public class AddTagToTask : IRequestHandler<AddTagToTaskInput, TaskTagOutput>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+    private readonly ITaskRepository _taskRepository;
+    private readonly ITagRepository _tagRepository;
+
+    public AddTagToTask(IUnitOfWork unitOfWork, IMapper mapper, ITaskRepository taskRepository, ITagRepository tagRepository)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+        _taskRepository = taskRepository;
+        _tagRepository = tagRepository;
+    }
+
+    public async Task<TaskTagOutput> Handle(AddTagToTaskInput input, CancellationToken cancellationToken)
+    {
+        var task = await _taskRepository.Get(input.TaskId, cancellationToken);
+
+        if (task is null)
+            return new TaskTagOutput(false, "Task not found");
+
+        var tag = await _tagRepository.Get(input.TagId, cancellationToken);
+
+        if (tag is null)
+            return new TaskTagOutput(false, "One or more tags not found");        
+
+        await _unitOfWork.Commit(cancellationToken);
+
+        return _mapper.Map<TaskTagOutput>(task);
+    }
+}
