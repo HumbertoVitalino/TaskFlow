984e14d5f376a5c46ec0156e1c73a2a168b2e9be
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-26 09:04:13 -0300

    feat: boundaries

---
+++ Core/UseCases/TaskUseCases/AddTagToTask/Boundaries/AddTagToTaskInput.cs
@@ -0,0 +1,6 @@
+﻿using Core.UseCases.TaskUseCases.Output;
+using MediatR;
+
+namespace Core.UseCases.TaskUseCases.AddTagToTask.Boundaries;
+
+public sealed record AddTagToTaskInput(int TaskId, int TagId) : IRequest<TaskTagOutput>;
---
+++ Core/UseCases/TaskUseCases/AddTagToTask/Mapper/AddTagToTaskMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Core.UseCases.TaskUseCases.AddTagToTask.Boundaries;
+using Core.UseCases.TaskUseCases.Output;
+
+namespace Core.UseCases.TaskUseCases.AddTagToTask.Mapper;
+
+public sealed class AddTagToTaskMapper : Profile
+{
+    public AddTagToTaskMapper()
+    {
+        CreateMap<AddTagToTaskInput, Entities.Task>();
+        CreateMap<Entities.Task, TaskTagOutput>()
+            .ForMember(dest => dest.Message, opt => opt.MapFrom(src => "Tags added successfully")); 
+    }
+}
