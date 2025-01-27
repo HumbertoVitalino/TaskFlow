707f035652ee8af0dbe113ae80612d7c806bf350
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-27 08:19:35 -0300

    refactor: boundaries

--- Core/UseCases/TaskUseCases/CreateTask/Boundaries/CreateTaskInput.cs
+++ Core/UseCases/TaskUseCases/CreateTask/Boundaries/CreateTaskInput.cs
@@ -1,6 +1,7 @@
-﻿using Core.UseCases.TaskUseCases.Output;
+﻿using Core.Entities;
+using Core.UseCases.TaskUseCases.Output;
 using MediatR;
 
 namespace Core.UseCases.TaskUseCases.Boundaries;
 
-public sealed record CreateTaskInput(string Title, string? Description, DateTime DueDate) : IRequest<TaskOutput>;
+public sealed record CreateTaskInput(string Title, string? Description, DateTime DueDate, int? TagId) : IRequest<TaskOutput>;
