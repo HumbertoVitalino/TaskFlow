079675de98281465722ab6131e76174bf32756e1
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-27 08:41:39 -0300

    feat: boundaries

---
+++ Core/UseCases/TagUseCases/UpdateTag/Boundaries/UpdateTagInput.cs
@@ -0,0 +1,6 @@
+﻿using Core.UseCases.TagUseCases.Output;
+using MediatR;
+
+namespace Core.UseCases.TagUseCases.UpdateTag.Boundaries;
+
+public sealed record UpdateTagInput(int Id, string Name, string? Description) : IRequest<TagOutput>;
