35a945e9a80b2ff118e86ea75a968397f4744e95
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-27 08:18:24 -0300

    remove dbcontext

--- Infra/Context/AppDbContext.cs
+++ Infra/Context/AppDbContext.cs
@@ -9,25 +9,4 @@ public class AppDbContext : DbContext
     public DbSet<Tag> Tags { get; set; }
     public DbSet<Core.Entities.Task> Tasks { get; set; }
     public DbSet<User> Users { get; set; }
-    public DbSet<TaskTag> TaskTags { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-
-        // Relacionamento muitos-para-muitos
-        modelBuilder.Entity<TaskTag>()
-            .HasKey(tt => new { tt.TaskId, tt.TagId });
-
-        modelBuilder.Entity<TaskTag>()
-            .HasOne(tt => tt.Task)
-            .WithMany(t => t.TaskTags)
-            .HasForeignKey(tt => tt.TaskId);
-
-        modelBuilder.Entity<TaskTag>()
-            .HasOne(tt => tt.Tag)
-            .WithMany(t => t.TaskTags)
-            .HasForeignKey(tt => tt.TagId);
-    }
-
 }
