e11a0d2e4db371103dfa3d16178d2fa3d1141de1
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-27 08:17:28 -0300

    removing: tasktag

--- Core/Entities/TaskTag.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Core.Entities;
-
-public sealed class TaskTag
-{
-    public int TaskId { get; set; }
-    public Entities.Task Task { get; set; }
-    public int TagId { get; set; }
-    public Tag Tag { get; set; }
-}
--- Core/UseCases/TaskUseCases/Output/TaskTagOutput.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace Core.UseCases.TaskUseCases.Output;
-
-public sealed record TaskTagOutput(bool Sucess, string Message);
