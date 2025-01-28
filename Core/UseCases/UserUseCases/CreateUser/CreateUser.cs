8866ccffc28ebe33da654f4f33211f5e5696ef5d
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-28 08:00:47 -0300

    feat: user usecase

---
+++ Core/UseCases/UserUseCases/CreateUser/CreateUser.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Core.Interfaces;
+using Core.UseCases.UserUseCases.CreateUser.Boundaries;
+using Core.UseCases.UserUseCases.Output;
+using MediatR;
+
+namespace Core.UseCases.UserUseCases.CreateUser;
+
+public class CreateUser : IRequestHandler<CreateUserInput, UserOutput>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateUser(IUserRepository userRepository, IMapper mapper, IUnitOfWork unitOfWork)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<UserOutput> Handle(CreateUserInput input, CancellationToken cancellationToken)
+    {
+        var user = _mapper.Map<User>(input);
+
+        _userRepository.Create(user);
+
+        await _unitOfWork.Commit(cancellationToken);
+
+        return _mapper.Map<UserOutput>(user);
+    }
+}
