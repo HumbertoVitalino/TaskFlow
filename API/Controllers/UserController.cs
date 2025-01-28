cf706f003eca1147d25df3f9d950d92e5c377497
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-28 08:01:01 -0300

    feat: user controller

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Core.UseCases.UserUseCases.CreateUser.Boundaries;
+using Core.UseCases.UserUseCases.Output;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly IMediator _mediatr;
+
+    public UserController(IMediator mediatr)
+    {
+        _mediatr = mediatr;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<UserOutput>> CreateUser(CreateUserInput input, CancellationToken cancellationToken)
+    {
+        var output = await _mediatr.Send(input, cancellationToken);
+        return Ok(output);
+    }
+}
