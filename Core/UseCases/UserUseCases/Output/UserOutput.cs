8623a753abbcd25de68eb1ffcf244f8f602f2940
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-28 08:00:54 -0300

    feat: useroutput

---
+++ Core/UseCases/UserUseCases/Output/UserOutput.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.UseCases.UserUseCases.Output;
+
+public class UserOutput
+{
+    public string Data { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public bool Status { get; set; } = true;
+}
