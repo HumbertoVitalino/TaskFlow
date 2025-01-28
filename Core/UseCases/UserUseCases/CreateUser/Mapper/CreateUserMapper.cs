3e168cf12ae5154e0997cb959c60ebc21d2fb323
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-28 08:00:38 -0300

    feat: user boundaries

---
+++ Core/UseCases/UserUseCases/CreateUser/Mapper/CreateUserMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Core.UseCases.UserUseCases.CreateUser.Boundaries;
+using Core.UseCases.UserUseCases.Output;
+
+namespace Core.UseCases.UserUseCases.CreateUser.Mapper;
+
+public sealed class CreateUserMapper : Profile
+{
+    public CreateUserMapper()
+    {
+        CreateMap<CreateUserInput, User>();
+        CreateMap<User, UserOutput>();
+    }
+}
