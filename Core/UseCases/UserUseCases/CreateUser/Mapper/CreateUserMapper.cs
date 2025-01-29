9c5b857739aeeafa3b6115e84cb5497baa45b6a9
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-29 08:20:45 -0300

    refactor: user mapper

--- Core/UseCases/UserUseCases/CreateUser/Mapper/CreateUserMapper.cs
+++ Core/UseCases/UserUseCases/CreateUser/Mapper/CreateUserMapper.cs
@@ -3,13 +3,18 @@ using Core.Entities;
 using Core.UseCases.UserUseCases.CreateUser.Boundaries;
 using Core.UseCases.UserUseCases.Output;
 
-namespace Core.UseCases.UserUseCases.CreateUser.Mapper;
-
 public sealed class CreateUserMapper : Profile
 {
     public CreateUserMapper()
     {
-        CreateMap<CreateUserInput, User>();
+        CreateMap<CreateUserInput, User>()
+            .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+            .ForMember(dest => dest.PasswordSalt, opt => opt.Ignore())
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+            .ForSourceMember(src => src.Confirmation, opt => opt.DoNotValidate());
+
         CreateMap<User, UserOutput>();
     }
 }
