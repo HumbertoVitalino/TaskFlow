599c50842d712c9fccac360746cfce9883f20d32
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-30 07:35:19 -0300

    refactor: output

--- Core/UseCases/UserUseCases/Output/UserOutput.cs
+++ Core/UseCases/UserUseCases/Output/UserOutput.cs
@@ -5,4 +5,5 @@ public class UserOutput
     public string Data { get; set; }
     public string Message { get; set; } = string.Empty;
     public bool Status { get; set; } = true;
+    public string? Token { get; set; }
 }
