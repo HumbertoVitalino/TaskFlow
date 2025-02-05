4e6c91405adc978b9dbf83d3fd640912ccd5662f
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-02-05 19:16:57 -0300

    feat: test

---
+++ UnitTests/TaskUseCasesTest/CreateTaskTest.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+
+namespace UnitTests.TaskUseCasesTest;
+
+public class CreateTaskTest : IClassFixture<CreateTaskTestFixture>
+{
+    private readonly CreateTaskTestFixture _fixture;
+
+    public CreateTaskTest(CreateTaskTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = "Handle should act with success")]
+    public void Handle_Should_Act_With_Success()
+    {
+        // Arrange
+        var input = _fixture.SetupValidInput();
+
+        // Act
+        var result = _fixture.HandleUseCaseAsync(input);
+
+        // Assert
+        result.Should().NotBeNull();
+    }
+
+    [Fact(DisplayName = "Handle should act with success without tagId")]
+    public void Handle_Should_Act_With_Success_Without_TagId()
+    {
+        // Arrange
+        var input = _fixture.SetupValidInputWithoutTag();
+
+        // Act
+        var result = _fixture.HandleUseCaseAsync(input);
+
+        // Assert
+        result.Should().NotBeNull();
+    }
+    
+}
