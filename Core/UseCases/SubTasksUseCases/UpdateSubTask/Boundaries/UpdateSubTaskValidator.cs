f99cb56e3ed44e9013f8ed3ff21ae966f12e06f7
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-02-17 20:46:13 -0300

    feat: boundaries

---
+++ Core/UseCases/SubTasksUseCases/UpdateSubTask/Boundaries/UpdateSubTaskValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Core.UseCases.SubTasksUseCases.UpdateSubTask.Boundaries;
+
+public class UpdateSubTaskValidator : AbstractValidator<UpdateSubTaskInput>
+{
+	public UpdateSubTaskValidator()
+	{
+		RuleFor(x => x.IdSubTask).NotEmpty();
+		RuleFor(x => x.Status).NotEmpty();
+	}
+}
