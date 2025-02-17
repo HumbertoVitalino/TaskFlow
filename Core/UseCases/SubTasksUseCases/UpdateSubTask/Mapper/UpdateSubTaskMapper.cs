a4a2d72d52dfdedb69e8c44d3d8c33cf5dae61cb
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-02-17 20:46:21 -0300

    feat: mapper

---
+++ Core/UseCases/SubTasksUseCases/UpdateSubTask/Mapper/UpdateSubTaskMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Core.UseCases.SubTasksUseCases.Output;
+using Core.UseCases.SubTasksUseCases.UpdateSubTask.Boundaries;
+
+namespace Core.UseCases.SubTasksUseCases.UpdateSubTask.Mapper;
+
+public sealed class UpdateSubTaskMapper : Profile
+{
+    public UpdateSubTaskMapper()
+    {
+        CreateMap<UpdateSubTaskInput, SubTask>();
+        CreateMap<SubTask, SubTaskOutput>();
+    }
+}
