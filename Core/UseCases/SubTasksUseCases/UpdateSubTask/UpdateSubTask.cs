b5b3decc390bc7b00bfb70fdd3c0ea21473750e7
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-02-17 20:45:58 -0300

    feat: usecase

---
+++ Core/UseCases/SubTasksUseCases/UpdateSubTask/UpdateSubTask.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Core.Enums;
+using Core.Interfaces;
+using Core.UseCases.SubTasksUseCases.Output;
+using Core.UseCases.SubTasksUseCases.UpdateSubTask.Boundaries;
+using MediatR;
+
+namespace Core.UseCases.SubTasksUseCases.UpdateSubTask;
+
+public class UpdateSubTask : IRequestHandler<UpdateSubTaskInput, SubTaskOutput>
+{
+    private readonly ISubTaskRepository _subTaskRepository;
+    private readonly ITaskRepository _taskRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public UpdateSubTask(ISubTaskRepository subTaskRepository, ITaskRepository taskRepository, IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _subTaskRepository = subTaskRepository;
+        _taskRepository = taskRepository;
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<SubTaskOutput> Handle(UpdateSubTaskInput input, CancellationToken cancellationToken)
+    {
+        var subtask = await _subTaskRepository.Get(input.IdSubTask, cancellationToken);
+
+        if (subtask == null)
+            throw new KeyNotFoundException($"SubTask with key {input.IdSubTask} not found.");
+                
+        subtask.Status = input.Status;
+        subtask.Title = input.Title;
+        subtask.DueDate = input.DueDate;
+
+        _subTaskRepository.Update(subtask);
+
+        var task = await _taskRepository.Get(subtask.Task.Id, cancellationToken);
+
+        if (task is not null)
+        {
+            if (task.SubTasks.Any(x => x.Status == StatusEnum.InProgress))
+            task.Status = StatusEnum.InProgress;
+
+            if (task.SubTasks.All(x => x.Status == StatusEnum.Completed))
+                task.Status = StatusEnum.Completed;
+        }
+
+        _taskRepository.Update(task);
+
+        await _unitOfWork.Commit(cancellationToken);
+
+        return _mapper.Map<SubTaskOutput>(subtask);
+    }
+}
