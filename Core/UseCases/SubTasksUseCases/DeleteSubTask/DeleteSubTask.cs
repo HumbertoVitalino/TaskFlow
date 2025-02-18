d79151f2dff5d0e6df1639548ac63688dc9bc5b1
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-02-18 00:57:27 -0300

    feat: usecase

---
+++ Core/UseCases/SubTasksUseCases/DeleteSubTask/DeleteSubTask.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Core.Interfaces;
+using Core.UseCases.SubTasksUseCases.DeleteSubTask.Boundaries;
+using Core.UseCases.SubTasksUseCases.Output;
+using MediatR;
+
+namespace Core.UseCases.SubTasksUseCases.DeleteSubTask;
+
+public class DeleteSubTask : IRequestHandler<DeleteSubTaskInput, SubTaskOutput>
+{
+    private readonly ISubTaskRepository _subTaskRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public DeleteSubTask(ISubTaskRepository subTaskRepository, IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _subTaskRepository = subTaskRepository;
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<SubTaskOutput> Handle(DeleteSubTaskInput input, CancellationToken cancellationToken)
+    {
+        var subtask = await _subTaskRepository.Get(input.Id, cancellationToken);
+
+        if (subtask is null)
+            return default!;
+
+        _subTaskRepository.Delete(subtask);
+        await _unitOfWork.Commit(cancellationToken);
+
+        return _mapper.Map<SubTaskOutput>(subtask);
+    }
+}
