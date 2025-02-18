75491d76ed5ee2e6b83d5a75821f6302ebe5c09e
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-02-18 00:57:13 -0300

    feat: boundaries

---
+++ Core/UseCases/SubTasksUseCases/DeleteSubTask/Boundaries/DeleteSubTaskValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Core.UseCases.SubTasksUseCases.DeleteSubTask.Boundaries;
+
+public class DeleteSubTaskValidator : AbstractValidator<DeleteSubTaskInput>
+{
+	public DeleteSubTaskValidator()
+	{
+		RuleFor(x => x.Id).NotEmpty();
+	}
+}
