b95d336438be69230ec68a456de316e6a56b0ecc
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-02-18 00:57:22 -0300

    feat: mapper

---
+++ Core/UseCases/SubTasksUseCases/DeleteSubTask/Mapper/DeleteSubTaskMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Core.UseCases.SubTasksUseCases.DeleteSubTask.Boundaries;
+using Core.UseCases.SubTasksUseCases.Output;
+
+namespace Core.UseCases.SubTasksUseCases.DeleteSubTask.Mapper;
+
+public sealed class DeleteSubTaskMapper : Profile
+{
+    public DeleteSubTaskMapper()
+    {
+        CreateMap<DeleteSubTaskInput, SubTask>();
+        CreateMap<SubTask, SubTaskOutput>();
+    }
+}
