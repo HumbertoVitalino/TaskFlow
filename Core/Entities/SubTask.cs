296cb43ca131d46ffb3277e206491985bb6189b1
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-02-25 21:45:12 -0300

    feat: entities

--- Core/Entities/SubTask.cs
+++ Core/Entities/SubTask.cs
@@ -7,7 +7,9 @@ public sealed class SubTask : BaseEntity
     public string Title { get; set; }
     public DateTime DueDate { get; set; }
     public StatusEnum Status { get; set; }
+    public int TaskId { get; set; }
     public Task Task { get; set; }
+    public int UserId { get; set; }
     public User User { get; set; }
 
     public SubTask() 
