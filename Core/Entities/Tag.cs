07b7da64c88ba1e64f0707f2522477016843c298
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-02-25 22:33:59 -0300

    refactor: tag entity

--- Core/Entities/Tag.cs
+++ Core/Entities/Tag.cs
@@ -5,5 +5,6 @@ public sealed class Tag : BaseEntity
     public string Name { get; set; }
     public string? Description { get; set; }
     public ICollection<Task> Tasks  { get; set; }
+    public int UserId { get; set; }
     public User User { get; set; }
 }
