151dbe586f95b393df644703827d853440686f6f
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-02-25 21:45:23 -0300

    feat: migrations

--- Infra/Context/AppDbContext.cs
+++ Infra/Context/AppDbContext.cs
@@ -6,8 +6,36 @@ namespace Infra.Context;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
     public DbSet<Tag> Tags { get; set; }
     public DbSet<Core.Entities.Task> Tasks { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<SubTask> SubTasks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Core.Entities.Task>()
+            .HasOne(t => t.User)
+            .WithMany(u => u.Tasks)
+            .HasForeignKey(t => t.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+        
+        modelBuilder.Entity<Core.Entities.Task>()
+            .HasOne(t => t.Tag)
+            .WithMany()
+            .HasForeignKey(t => t.TagId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        modelBuilder.Entity<SubTask>()
+            .HasOne(st => st.Task)
+            .WithMany(t => t.SubTasks)
+            .HasForeignKey(st => st.TaskId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Core.Entities.Task>()
+            .Property(t => t.Status)
+            .HasConversion<string>();
+    }
 }
---
+++ Infra/Migrations/20250226004342_FixRelations.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Tags_TagId",
+                table: "Tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_UserId",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TagId1",
+                table: "Tasks",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_TagId1",
+                table: "Tasks",
+                column: "TagId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Tags_TagId",
+                table: "Tasks",
+                column: "TagId",
+                principalTable: "Tags",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Tags_TagId1",
+                table: "Tasks",
+                column: "TagId1",
+                principalTable: "Tags",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_UserId",
+                table: "Tasks",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Tags_TagId",
+                table: "Tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Tags_TagId1",
+                table: "Tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_UserId",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_TagId1",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "TagId1",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Tasks",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Tags_TagId",
+                table: "Tasks",
+                column: "TagId",
+                principalTable: "Tags",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_UserId",
+                table: "Tasks",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
